13969a31709103005dbfb9f55a5a5b925012d4af
J1372 <95717261+J1372@users.noreply.github.com>
2022-11-30 17:00:01 -0700

    More prototypes

--- games.ts
+++ games.ts
@@ -4,8 +4,22 @@
     Each number is in range of [0, 63] and represents a square on the board.
 */
 export interface Move {
-    from: number,
-    to: number,
+    from: number;
+    to: number;
+
+    // A one character representation of the piece that was moved.
+    pieceMoved: string;
+
+    // May be empty if move did not capture
+    pieceCaptured: string;
+
+    // A one-character representation of the promotion piece.
+    // May be empty if no promotion occurred.
+    promotedTo: string;
+
+    // function to get a string rep of the move.
+
+
 }
 
 export class TimeControl {
@@ -45,6 +59,7 @@ export class Game {
     uuid: string; // length = 16
     white: Player;
     black: Player;
+    board: Board;
     moves: Move[]; // move history.
 
     // Date the game actually started (both users could make moves).
@@ -61,7 +76,7 @@ export class Game {
         this.started = new Date(); // could use Date.now instead
 
 
-        switch (description.hostPlayAs){
+        switch (description.hostPlayAs) {
             case undefined: {
                 const hostPlaysWhite = Math.random() <= 0.5;
                 if (hostPlaysWhite) {
@@ -86,6 +101,8 @@ export class Game {
         }
 
         this.curTurn = Color.White;
+
+        this.board = new Board(8, 8);
         this.moves = new Array<Move>();
 
     }
@@ -118,4 +135,72 @@ export class GamePost {
         this.hostPlayAs = hostPlayAs;
     }
 
+}
+
+interface Square {
+    row: number;
+    col: number;
+}
+
+interface Piece {
+    color: Color;
+
+    /**
+     * Get a list of squares that the piece can move to.
+     * @param pos Square the ppiece is currently on.
+     * @param board The board the piece is on.
+     */
+    getMoves(pos: Square, board: Board): Array<Square>;
+
+    /**
+     * Get possible piece promotions for this piece if it were to move to a square on the given board.
+     * @param onMoveTo Square to move to on the board.
+     * @param board The board the piece is on.
+     */
+    getPromotionsOnMove(onMoveTo: Square, board: Board): Array<Piece>;
+}
+
+class Board {
+    board: Array<Piece | null>;
+
+    rows: number;
+    cols: number;
+
+    // Currently only supports the standard chess board piece setup.
+    constructor(rows: number, cols: number /*config?: Array<[Square, Piece]>*/) {
+        this.rows = rows;
+        this.cols = cols;
+        this.board = new Array<Piece>(rows * cols);
+
+        // if config -> do config setup else do standard setup
+
+        this.standardBoardSetup()
+    }
+
+    /**
+     * Sets the board up with the standard chess setup.
+     */
+    standardBoardSetup() {
+        function pawnRow(row: number, color: Color, board: Board) {
+            for (let i = 0; i < board.cols; ++i) {
+                board.board[i] = null;
+            }
+        }
+
+        pawnRow(1, Color.White, this);
+        pawnRow(6, Color.White, this);
+
+        // knights, bishops, rooks, queen, and king.
+    }
+
+    /**
+     * Gets the piece at a square on the board.
+     * @param row 
+     * @param col 
+     * @returns The piece at the given position or null.
+     */
+    getPiece(row: number, col: number) {
+        const index = row * this.cols + col; // 0 index
+        return this.board[index];
+    }
 }
