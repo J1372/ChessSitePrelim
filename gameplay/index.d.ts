1e9c66f89152afa73efb138833f15119f8074671
J1372 <95717261+J1372@users.noreply.github.com>
2024-03-15 11:38:43 -0700

    Move to src folder.

---
+++ gameplay/index.d.ts
@@ -0,0 +1,7 @@
+export * from './build/board';
+export * from './build/game';
+export * from './build/color';
+export * from './build/game_post';
+export * from './build/move';
+export * from './build/square';
+export * from './build/pieces/piece_factory';
